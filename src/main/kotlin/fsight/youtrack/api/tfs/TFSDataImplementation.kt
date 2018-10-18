93e8a243e2074d91d1ba632715e5c7818410690b
dmitriy.khlopin <qwerten85@gmail.com>
2018-10-18 17:22:45 +0500

    Created controller, service and implementation for data export from TFS to YT.

--- src/main/kotlin/fsight/youtrack/api/tfs/TFSDataImplementation.kt
+++ src/main/kotlin/fsight/youtrack/api/tfs/TFSDataImplementation.kt
@@ -1,9 +1,20 @@
 package fsight.youtrack.api.tfs
 
+import com.google.gson.Gson
+import fsight.youtrack.AUTH
+import fsight.youtrack.NEW_ROOT_REF
 import fsight.youtrack.generated.jooq.tables.TfsWi.TFS_WI
 import fsight.youtrack.models.TFSItem
 import org.jooq.DSLContext
 import org.springframework.stereotype.Service
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
 
 @Service
 class TFSDataImplementation(private val dslContext: DSLContext) : TFSDataService {
@@ -81,7 +92,30 @@ class TFSDataImplementation(private val dslContext: DSLContext) : TFSDataService
                 TFS_WI.ITERATION_PATH.`as`("iteration_path"),
                 TFS_WI.ITERATION_NAME.`as`("iteration_name")
         ).from(TFS_WI).where(TFS_WI.ID.eq(id)).fetchOneInto(TFSItem::class.java)
-        println(item)
+        val id2 = PostIssueRetrofitService.create().createIssue(AUTH, Gson().toJson(YTIssue(Project("0-15"), "test", "test"))).execute()
+        println("readable id = ${id2.body()} - ${id2.errorBody()}")
         return item
+    }
+}
+
+
+data class Project(val id: String)
+data class YTIssue(val project: Project, val summary: String, val description: String)
+/*data class ReadableId(val idReadable: String)*/
+
+interface PostIssueRetrofitService {
+    @Headers("Accept: application/json", "Content-Type: application/json;charset=UTF-8", "Access-Control-Expose-Headers: Location")
+    @POST("issues?fields=idReadable")
+    fun createIssue(
+            @Header("Authorization") auth: String,
+            @Body model: String): Call<String>
+
+    companion object Factory {
+        fun create(): PostIssueRetrofitService {
+            /*val gson = GsonBuilder().setLenient().create()*/
+            /*val retrofit = Retrofit.Builder().baseUrl(NEW_ROOT_REF).addConverterFactory(GsonConverterFactory.create(gson)).build()*/
+            val retrofit = Retrofit.Builder().baseUrl(NEW_ROOT_REF).addConverterFactory(ScalarsConverterFactory.create()).build()
+            return retrofit.create(PostIssueRetrofitService::class.java)
+        }
     }
 }
