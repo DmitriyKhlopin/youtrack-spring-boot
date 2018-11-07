f8fd5b966d2fb473ef50dcd8114e83db2e75e2a2
dmitriy.khlopin <qwerten85@gmail.com>
2018-11-07 15:50:07 +0500

    Implemented tasks creation.

--- src/main/kotlin/fsight/youtrack/api/tfs/TFSDataImplementation.kt
+++ src/main/kotlin/fsight/youtrack/api/tfs/TFSDataImplementation.kt
@@ -5,6 +5,8 @@ import fsight.youtrack.AUTH
 import fsight.youtrack.NEW_ROOT_REF
 import fsight.youtrack.etl.bundles.v2.BundleValue
 import fsight.youtrack.generated.jooq.tables.BundleValues.BUNDLE_VALUES
+import fsight.youtrack.generated.jooq.tables.TfsLinks.TFS_LINKS
+import fsight.youtrack.generated.jooq.tables.TfsTasks.TFS_TASKS
 import fsight.youtrack.generated.jooq.tables.TfsWi.TFS_WI
 import fsight.youtrack.generated.jooq.tables.Users.USERS
 import fsight.youtrack.models.TFSRequirement
@@ -22,6 +24,7 @@ import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import java.sql.Timestamp
 
 @Service
 class TFSDataImplementation(private val dslContext: DSLContext) : TFSDataService {
@@ -131,21 +134,82 @@ class TFSDataImplementation(private val dslContext: DSLContext) : TFSDataService
 
     override fun postItemToYouTrack(id: Int): ResponseEntity<Any> {
         val item = getItemById(id)
-        println(item)
-        val postableItem = getPostableItem(item)
+        val postableItem = getPostableRequirement(item)
         println(postableItem)
         val id2 = PostIssueRetrofitService.create().createIssue(AUTH, postableItem).execute()
-        println("readable id = ${id2.body()} - ${id2.errorBody()}")
+        /*println("readable id = ${id2.body()} - ${id2.errorBody()}")*/
+        val idReadable = Gson().fromJson(id2.body(), ReadableId::class.java)
+
+        if (id2.errorBody() == null) getTasks(id, idReadable.idReadable ?: "")
         return ResponseEntity.status(HttpStatus.OK).body(id2)
     }
 
+
+    data class TFSTask(
+            val id: Int,
+            val rev: Int,
+            val state: String? = null,
+            val type: String? = null,
+            val createDate: Timestamp,
+            val discipline: String? = null,
+            val developmentEc: Int,
+            val testEc: Int,
+            val project: String? = null,
+            val projectNodeName: String? = null,
+            val projectPath: String? = null,
+            val areaName: String? = null,
+            val areaPath: String? = null,
+            val iterationPath: String? = null,
+            val iterationName: String? = null,
+            val title: String? = null,
+            val description: String? = null
+    )
+
+
+    fun getTasks(requirementId: Int, parentId: String) {
+        val r = dslContext.select(
+                TFS_TASKS.ID,
+                TFS_TASKS.REV,
+                TFS_TASKS.STATE,
+                TFS_TASKS.TYPE,
+                TFS_TASKS.CREATE_DATE.`as`("createDate"),
+                TFS_TASKS.DISCIPLINE,
+                TFS_TASKS.DEVELOPMENT_EC.`as`("developmentEc"),
+                TFS_TASKS.TEST_EC.`as`("testEc"),
+                TFS_TASKS.PROJECT,
+                TFS_TASKS.PROJECT_NODE_NAME.`as`("projectNodeName"),
+                TFS_TASKS.PROJECT_PATH.`as`("projectPath"),
+                TFS_TASKS.AREA_NAME.`as`("areaName"),
+                TFS_TASKS.AREA_PATH.`as`("areaPath"),
+                TFS_TASKS.ITERATION_PATH.`as`("iterationPath"),
+                TFS_TASKS.ITERATION_NAME.`as`("iterationName"),
+                TFS_TASKS.TITLE,
+                TFS_TASKS.DESCRIPTION
+        )
+                .from(TFS_TASKS)
+                .leftJoin(TFS_LINKS).on(TFS_TASKS.ID.eq(TFS_LINKS.TARGET_ID))
+                .where(TFS_LINKS.SOURCE_ID.eq(requirementId))
+                .fetchInto(TFSTask::class.java).map { item -> getPostableTask(item) }
+        r.forEach {
+            /*println(it)*/
+            val id2 = PostIssueRetrofitService.create().createIssue(AUTH, it).execute()
+            println("readable id = ${id2.body()} - ${id2.errorBody()}")
+            val idReadable = Gson().fromJson(id2.body(), ReadableId::class.java)
+            val command = Gson().toJson(YouTrackCommand(issues = arrayListOf(IssueIn(id = idReadable.id)), silent = true, query = "подзадача $parentId"))
+            println(command)
+            val r2 = PostCommandService.create().createIssue(AUTH, command).execute()
+            println("readable id = ${r2.body()} - ${r2.errorBody()}")
+        }
+    }
+
+
     override fun toJson(id: Int): ResponseEntity<Any> {
         val item = getItemById(id)
-        val postableItem = getPostableItem(item)
+        val postableItem = getPostableRequirement(item)
         return ResponseEntity.status(HttpStatus.OK).body(postableItem)
     }
 
-    fun getPostableItem(item: TFSRequirement): String {
+    fun getPostableRequirement(item: TFSRequirement): String {
         val proposalQuality = customFieldValues.asSequence().filter {
             it.fieldName == "Proposal quality" && it.name == (item.proposalQuality ?: "Average")
         }.first().let { it -> FieldValue(id = it.fieldId, `$type` = types[it.`$type`], value = ActualValue(id = it.id, name = it.name)) }
@@ -164,26 +228,46 @@ class TFSDataImplementation(private val dslContext: DSLContext) : TFSDataService
         val u = users.firstOrNull { it.email == item.productManager }
         val assignee = FieldValue(id = "86-16", `$type` = "jetbrains.charisma.customfields.complex.user.SingleUserIssueCustomField", value = ActualValue(id = u?.id
                 ?: "1-1", name = u?.fullName ?: "admin"))
-        val estimation = PeriodValue(id = "100-17", `$type` = "jetbrains.youtrack.timetracking.periodField.PeriodIssueCustomField", value = ActualPeriodValue(`$type` = "jetbrains.youtrack.timetracking.periodField.PeriodValue", minutes = 360))
+        /*val estimation = PeriodValue(id = "100-17", `$type` = "jetbrains.youtrack.timetracking.periodField.PeriodIssueCustomField", value = ActualPeriodValue(`$type` = "jetbrains.youtrack.timetracking.periodField.PeriodValue", minutes = 360))*/
         return Gson().toJson(
                 IssueIn(
                         project = Project(id = "0-15"),
                         summary = item.title,
                         description = "PD:\n${Jsoup.parse(item.problemDescription).text()} \n\nPC:\n${Jsoup.parse(item.proposedChange).text()} \n\nER:\n${Jsoup.parse(item.expectedResult).text()}",
-                        fields = arrayListOf(type,proposalQuality, pmAccepted, dmAccepted, areaName, assignee, estimation))
+                        fields = arrayListOf(type, proposalQuality, pmAccepted, dmAccepted, areaName, assignee))
         )
     }
 
+    fun getPostableTask(item: TFSTask): String {
+        val type = customFieldValues.asSequence().filter {
+            it.fieldName == "Type" && it.name == (item.type ?: "Task")
+        }.first().let { it -> FieldValue(id = it.fieldId, `$type` = types[it.`$type`], value = ActualValue(id = it.id, name = it.name)) }
+
+        val areaName = customFieldValues.asSequence().filter {
+            it.fieldName == "Area name" && it.name == item.areaName
+        }.firstOrNull().let { it -> if (it !== null) FieldValue(id = it.fieldId, `$type` = types[it.`$type`], value = ActualValue(id = it.id, name = it.name)) else null }
+        val estimationDev = PeriodValue(id = "100-17", `$type` = "jetbrains.youtrack.timetracking.periodField.PeriodIssueCustomField", value = ActualPeriodValue(`$type` = "jetbrains.youtrack.timetracking.periodField.PeriodValue", minutes = (item.testEc + item.developmentEc) * 60))
+        return Gson().toJson(
+                IssueIn(
+                        project = Project(id = "0-15"),
+                        summary = item.title,
+                        description = Jsoup.parse(item.description).text(),
+                        fields = arrayListOf(type, areaName, estimationDev).filterNotNull() as ArrayList<Any>
+                ))
+    }
+
+    data class YouTrackCommand(var issues: ArrayList<IssueIn>, var silent: Boolean = false, var query: String = "")
+    data class ReadableId(val id: String? = null, val idReadable: String? = null)
     data class ActualPeriodValue(val `$type`: String?, val minutes: Int?)
     data class PeriodValue(val id: String?, val `$type`: String?, val value: ActualPeriodValue?)
     data class ActualValue(val id: String?, val name: String?)
     data class FieldValue(val id: String?, val `$type`: String?, val value: ActualValue?)
-    data class IssueIn(var description: String? = null, var fields: ArrayList<Any>? = null, var project: Project? = null, var summary: String? = null)
+    data class IssueIn(var description: String? = null, var fields: ArrayList<Any>? = null, var project: Project? = null, var summary: String? = null, var id: String? = null)
 }
 
 interface PostIssueRetrofitService {
     @Headers("Accept: application/json", "Content-Type: application/json;charset=UTF-8")
-    @POST("issues?fields=idReadable")
+    @POST("issues?fields=idReadable,id")
     fun createIssue(
             @Header("Authorization") auth: String,
             @Body model: String): Call<String>
@@ -192,6 +276,21 @@ interface PostIssueRetrofitService {
         fun create(): PostIssueRetrofitService {
             val retrofit = Retrofit.Builder().baseUrl(NEW_ROOT_REF).addConverterFactory(ScalarsConverterFactory.create()).build()
             return retrofit.create(PostIssueRetrofitService::class.java)
+        }
+    }
+}
+
+interface PostCommandService {
+    @Headers("Accept: application/json", "Content-Type: application/json;charset=UTF-8")
+    @POST("commands")
+    fun createIssue(
+            @Header("Authorization") auth: String,
+            @Body model: String): Call<String>
+
+    companion object Factory {
+        fun create(): PostCommandService {
+            val retrofit = Retrofit.Builder().baseUrl(NEW_ROOT_REF).addConverterFactory(ScalarsConverterFactory.create()).build()
+            return retrofit.create(PostCommandService::class.java)
         }
     }
 }
